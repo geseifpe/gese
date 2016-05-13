b272646f81b5de80cd61cac72d5f42670369d6a6
nilson <njr1466@gmail.com>
2016-05-13 20:21:34 -0300

    quarto commit

---
+++ src/br/com/gese/testes/TesteLogin.java
@@ -0,0 +1,14 @@
+package br.com.gese.testes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteLogin {
+
+	@Test
+	public void testar(){
+		Assert.assertEquals(1,1);
+	}
+	
+	
+}
