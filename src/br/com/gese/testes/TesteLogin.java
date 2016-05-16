36e6faa783cf5e2f1eeb27e4ce4bdf89ac1f0a90
nilson <geseifpe>
2016-05-16 14:28:27 -0300

    sexto commit

--- src/br/com/gese/testes/TesteLogin.java
+++ src/br/com/gese/testes/TesteLogin.java
@@ -2,13 +2,22 @@ package br.com.gese.testes;
 
 import org.junit.Assert;
 
+import br.com.gese.dao.AlunoDao;
+import br.com.gese.dao.UsuarioDao;
+import br.com.gese.model.Aluno;
+import br.com.gese.model.Usuario;
+import br.com.gese.util.Criptografia;
 import junit.framework.TestCase;
 
 public class TesteLogin extends TestCase {
 
 	
-	public void testar(){
-		Assert.assertEquals(2,2);
+	public void testarLogin(){
+		AlunoDao daoAluno= new AlunoDao();
+		UsuarioDao daoUsuario = new UsuarioDao();
+		Aluno aluno = daoAluno.getAlunoId("03107383422");
+        Usuario usuario = daoUsuario.login(aluno.getCpf(), Criptografia.md5("123456"));
+		Assert.assertNotEquals(usuario,null);
 	}
 	
 	public void testar1(){
